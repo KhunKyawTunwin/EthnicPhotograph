d634a2fc60678741ceb05713fd22dbddf0d47554
KhunKyawTunwin <66564687+KhunKyawTunwin@users.noreply.github.com>
2023-02-05 11:20:06 +0630

    Rotate 360Deg Updated

--- main.js
+++ main.js
@@ -5,3 +5,17 @@ window.addEventListener("scroll", () => {
     .querySelector("nav")
     .classList.toggle("window-scrolled", window.scrollY > 0);
 });
+
+// Contact Button (Circular text buttons)
+
+const textButtons = document.querySelectorAll(".contact__btn");
+textButtons.forEach((textButton) => {
+  let text = textButton.querySelector("p");
+  text.innerHTML = text.innerHTML
+    .split("")
+    .map(
+      (character, index) =>
+        `<span style="transform: rotate(${index * 12}deg)">${character}</span>`
+    )
+    .join("");
+});
